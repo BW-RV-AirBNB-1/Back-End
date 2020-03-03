166d3f2ee98453e0584b0661292c0c1b19336c1d
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-03-03 13:57:07 -0500

    [Add] Completed get all, and add listing endpoints. tested on postman. working

--- listings/listing-model.js
+++ listings/listing-model.js
@@ -7,7 +7,23 @@ module.exports = {
 }
 
 function all(){
-
+    return db('listings')
+    .join('users', 'listings.user_id', 'users.id' )
+    .join('states', 'listings.state_id', 'states.id')
+    .select( 
+        'listings.id',
+        'title', 
+        'description', 
+        'price_per_day', 
+        'photo_url',
+        'latitude',
+        'longitude',
+        'username as owner',
+        'is_land_owner as land_owner',
+        'state_name as state', 
+        'state_abbreviation as state_abbrv'
+    )
+    .orderBy('listings.id');
 }
 
 function add(listing){
--- listings/listings-router.js
+++ listings/listings-router.js
@@ -2,7 +2,19 @@ const router = require('express').Router();
 const Listings = require('./listing-model');
 
 router.get('/', (req, res) =>{
-    res.status(200).json({message: "working"});
+    Listings.all()
+    .then(listing => {
+        res.status(200).json(listing)
+    })
+    .catch(({name, message, stack, code}) => {
+        res.status(500).json({ 
+            error: 'There was an getting the listings.',
+            name: name,
+            message: message,
+            stack: stack,
+            code: code
+        });
+    });
 });
 
 router.post('/', (req, res) => {
