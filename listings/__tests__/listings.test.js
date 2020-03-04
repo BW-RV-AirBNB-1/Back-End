ec7da3974c174ace1d4c571a00ce53c8ca8ac4bc
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-03-04 11:00:08 -0500

    [Updates] Updated tests truncation of data. all return passed

--- listings/__tests__/listings.test.js
+++ listings/__tests__/listings.test.js
@@ -1,48 +1,45 @@
-// const request = require('supertest');
-// const server = require('../../api/server');
-// const db = require('../../data/connection');
+const request = require('supertest');
+const server = require('../../api/server');
+const db = require('../../data/connection');
 
-// describe("Listings Router", () => {
+describe("Listings Router", () => {
 
-    
+    describe("GET /", () =>{
 
-//     afterEach(async () => {
-//         await db.raw('TRUNCATE TABLE users RESTART IDENTITY CASCADE')
-//         await db.seed.run()
-//     });
+        let token = null;
 
-//     describe("GET /", () =>{
+        beforeEach( async () => {
+            await db.seed.run('testing');
 
-//         let token = null;
-//         beforeEach( (done) => {
-//             const res = request(server).post('/api/login')
-//             .send({
-//                 "username": "testyMcTesty",
-//                 "password": "password12345",
-//                 "is_land_owner": false
-//             })
-//             .end( (err, res) => {
-//                 token = res.body.token
-//                 done();
-//             });
-//         });
+            const res = await request(server).post('/api/login')
+            .send({
+                "username": "testyMcTesty",
+                "password": "password12345",
+                "is_land_owner": false
+            })
 
-//         it("It should return 200 OK", async () => {
-            
-//             const res = await request(server).get('/api/listings')
-//             .set('Authorization', 'Bearer' + token)
+            token = res.body.token;
+        })
 
-//             expect(res.status).toBe(200);
-//         });
+        // afterEach(async () => {
+        //     await db.raw('TRUNCATE TABLE users RESTART IDENTITY CASCADE')
+        // });
 
-//         it("It should return json formatted body", async () => {
-//             const res = await request(server).get('/api/listings')
+        it("It should return 200 OK", async () => {
+            const res = await request(server).get('/api/listings')
+            .set('Authorization', `${token}`);
+        
+            expect(res.status).toBe(200);
+        });
 
-//             expect(res.type).toMatch(/json/i);
-//         });
+        it("It should return json formatted body", async () => {
+            const res = await request(server).get('/api/listings')
+            .set('Authorization', token);
 
-//     });
+            expect(res.type).toMatch(/json/i);
+        });
 
+    });
 
-// });
+});
 
